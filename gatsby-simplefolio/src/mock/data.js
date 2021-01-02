0a7261f6a582e9145c2009a9be987d15363305a8
Larry Castillo <larrycastillo2009@gmail.com>
2021-01-01 20:22:39 -0600

    about me

--- gatsby-simplefolio/src/mock/data.js
+++ gatsby-simplefolio/src/mock/data.js
@@ -18,10 +18,13 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'LarryCastilloHeadShot2.JPG',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'As new technologies emerge, developers have to remain sharp and update on a variety of languages and industry standards. With that in mind, developers are slowly realizing that they can no longer specify in a particular side of development, be it front-end or back-end. I like to think of my self as a jack of trades on an endless journey to learn and stay update to date on the latest standards and technnoloies, a true full-stack developer. ',
+  paragraphTwo:
+    'I have experience with using Java for various means, integrating various APIs into a web application with Javascript, managing databases using MySQL, working in a team environment using Agile methodology in a remote environment, using various distributions of Linux to any end, and I am also fluent in HTML and CSS along with the Bootstrap Framework. At the moment I am looking to add React.js to that list.',
+  paragraphThree:
+    'If you would like to chat about anything you see here, work on a project todether, or are an employer looking for your next team member, please email me down below and I will get back to you as soon as I can. See ya around the web!',
+  // resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
